46e15bad0a0ba1c4de48c0fef1f4a0d412313126
Amy Snarr <amy.snarr16@gmail.com>
2022-11-11 09:00:59 -0700

    when you click the start button, question shows and the answers are in the buttons

--- assets/script.js
+++ assets/script.js
@@ -5,19 +5,27 @@ var startQuizBtn = document.getElementById("start-quiz-btn")
 var timeLeft = document.getElementById("time-left");
 
 var questionDiv = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title")
+var choiceA = document.getElementById("btn1")
+var choiceB = document.getElementById("btn2")
+var choiceC = document.getElementById("btn3")
+var choiceD = document.getElementById("btn4")
 
 var scoreDiv = document.getElementById("score");
 
 var scoreboardDiv = document.getElementById("scoreboard");
 
+questionIndex = 0;
+
 // Set of questions
 const questions = [
     {
-        question: "inside which HTML element do we put the JavaScript?",
+        question: "Inside which HTML element do we put the JavaScript?",
         choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
         answer: "d. <script>"
     }
 ]
+
 
 // Begins the quiz by grabbing questions and starting timer
 function newQuiz() {
@@ -36,6 +44,19 @@ function newQuiz() {
             clearInterval(startTimer);
         }
     }, 1000);
+
+    nextQuestion();
 }
 
+// Display questions and answer options
+
+function nextQuestion() {
+    questionTitle.textContent = questions[questionIndex].question;
+    choiceA.textContent = questions[questionIndex].choices[0];
+    choiceB.textContent = questions[questionIndex].choices[1];
+    choiceC.textContent = questions[questionIndex].choices[2];
+    choiceD.textContent = questions[questionIndex].choices[3];
+}
+
+// Event listeners
 startQuizBtn.addEventListener("click", newQuiz)
