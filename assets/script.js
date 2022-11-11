c0cd1d060cbe16b9cc2859848f237fcc361df763
Amy Snarr <amy.snarr16@gmail.com>
2022-11-11 08:53:22 -0700

    when you click the start button, the timer begins

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
+// Grab reference to elements
 var startDiv = document.getElementById("start");
 var startQuizBtn = document.getElementById("start-quiz-btn")
+
+var timeLeft = document.getElementById("time-left");
 
 var questionDiv = document.getElementById("questions");
 
@@ -7,6 +10,7 @@ var scoreDiv = document.getElementById("score");
 
 var scoreboardDiv = document.getElementById("scoreboard");
 
+// Set of questions
 const questions = [
     {
         question: "inside which HTML element do we put the JavaScript?",
@@ -15,12 +19,23 @@ const questions = [
     }
 ]
 
+// Begins the quiz by grabbing questions and starting timer
 function newQuiz() {
     startDiv.style.display = "none";
     questionDiv.style.display = "block";
     scoreDiv.style.display = "none";
     scoreboardDiv.style.display = "none"
 
+    var totalTime = 20
+    timeLeft.textContent = totalTime;
+
+    var startTimer = setInterval(function() {
+        totalTime--;
+        timeLeft.textContent = totalTime;
+        if (totalTime <= 0) {
+            clearInterval(startTimer);
+        }
+    }, 1000);
 }
 
 startQuizBtn.addEventListener("click", newQuiz)
